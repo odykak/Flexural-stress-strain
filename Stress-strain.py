10f2bfa98fef0a5727db6a0336b16df6d84511b2
odykak <99960828+odykak@users.noreply.github.com>
2022-06-07 13:24:47 +0300

    Update Stress-strain.py

--- Stress-strain.py
+++ Stress-strain.py
@@ -13,27 +13,14 @@ import matplotlib as mpl
 import seaborn as sns
 import glob
 import os
-
 from scipy.optimize import curve_fit
 from sklearn.linear_model import LinearRegression
-
 from io import StringIO
 from natsort import natsorted
 
-#Appearance stuff (colors, font size etc)
-
-#My colors
-# mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['#c3121e', '#0348a1', '#ffb01c', '#027608', '#0193b0', '#9c5300', '#949c01', '#7104b5'])
-#                                                       0sangre,   1neptune,  2pumpkin,  3clover,   4denim,    5cocoa,    6cumin,    7berry3
-
-
-# mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['#e41a1c','#377eb8', '#ff7f00', '#4daf4a','#f781bf', '#a65628', '#984ea3','#999999', '#dede00'])
 
 # Nature colors
 mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['0C5DA5', 'FF2C00', 'FF9500', '00B945', '845B97', '474747', '9e9e9e'])
-
-#Seaborn colors
-# colors=sns.color_palette("rocket",3)
 
 # Font size. Dictionary taking numerous parameters.
 plt.rcParams.update({'font.size' : 12})
@@ -42,7 +29,7 @@ plt.rcParams.update({'font.size' : 12})
 thickness=[2.93666288,2.883306388,2.966662919,2.963329587,2.933329548,2.914995712,2.966662919,2.973329599,2.926572111,2.893329496] #d in mm
 width=[11.8633324,11.88666573,11.73333239,11.92666573,11.97666574,11.92666573,11.8533324,11.74999716,12.03999723,12.03999723] #b in mm
 #Span length in mm
-L=40.28
+L=40.28 #span
 
 # Gets current working directory (cwd)
 cwd=os.getcwd()
@@ -105,43 +92,22 @@ for index,dataframe in enumerate(data):
 for index,dataframe in enumerate(data):
     dataframe['Stress']=-1000*(3*dataframe['Force']*L)/(2*width[index]*(thickness[index]**2))
 
-#Drop lines for cropped data. Second line doesnt work. Might as well place them in plotting. #whyf tf would i place them in plotting??
-# for dataframe in data:
-# #    # dataframe=dataframe[0:250]
-#     dataframe=dataframe.drop(dataframe.index[6000:],inplace=True)
-
 # Insert in the for loop to plt in different graph. If you do so, pass the colors at the beginning of the script as a list if you wish to get different ones for each line.
 fig = plt.figure(figsize=(5,5),dpi=300)
 
 #Markers tuple to get different marker for each line
 markers=('o', 's', 'v', '^', '<', '>', '8', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'X')
 
-#Drop lines for cropped data. Might as well place them in plotting loop.
-# for dataframe in data:
-    # dataframe=dataframe[0:250]
-    # dataframe=dataframe.drop(dataframe.index[6000:],inplace=True)
 
 # #Plot in the same graph. Can as well locate columns with name but .iloc is useful af. Do as I say, not as I do.
 for index, dataframe in enumerate(data):
     # fig = plt.figure(figsize=(5,5),dpi=300)
     plt.plot(data[index]['Strain'],data[index]['Stress'],label=file_list[index],linestyle='-', marker=markers[index], mfc='w', markersize=8)
-  
-# Axes limits. Adjust accordingly.
-# plt.xlim([0.0012,0.009])
-# plt.ylim([-2,15])
-
+    
 # Ticks
 plt.minorticks_on() #uses minor ticks
-# plt.tick_params(direction='in',right=False, top=False) #Obsolete with following lines. Places MAJOR TICKS only on sides & handles direction. Avoid using it.
-# plt.tick_params(labelsize=10) #Changes values' font, which we locked earlier.
-# plt.tick_params(labelbottom=True, labeltop=False, labelright=False, labelleft=True) #Turns on/off axis values on all 4 sides
-# xticks = np.arange(1, 100,10) #Numpy to place ticks in certain positions X axis
-# yticks = np.arange(0,160.1,4) #Numpy to place ticks in certain positions Y axis
- 
 plt.tick_params(direction='in',which='minor', length=2, bottom=True, top=False, left=True, right=False) #Properly handles minor ticks
 plt.tick_params(direction='in',which='major', length=4, bottom=True, top=False, left=True, right=False)# Bot/left by default True if memory serves me. All change with =False though.
-#plt.xticks(xticks) #Plots ticks
-#plt.yticks(yticks) #Plots ticks
 
 #Plots grid in the background
 plt.grid(True,linestyle='dashed', linewidth='0.3', color='grey',alpha=0.8)
@@ -176,21 +142,14 @@ import matplotlib as mpl
 import seaborn as sns
 import glob
 import os
-
 from scipy.optimize import curve_fit
 from sklearn.linear_model import LinearRegression
-
 from io import StringIO
 from natsort import natsorted
 
-#Appearance stuff
-#My colors
-# mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['#c3121e', '#0348a1', '#ffb01c', '#027608', '#0193b0', '#9c5300', '#949c01', '#7104b5'])
-#                                                       0sangre,   1neptune,  2pumpkin,  3clover,   4denim,    5cocoa,    6cumin,    7berry3
 # Nature colors
 mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=['0C5DA5', 'FF2C00', 'FF9500', '00B945', '845B97', '474747', '9e9e9e'])
 #Seaborn colors
-# colors=sns.color_palette("rocket",3)
 # Font size. Dictionary taking numerous parameters.
 plt.rcParams.update({'font.size' : 12})
 
@@ -208,7 +167,6 @@ data = [pd.read_csv(file,names=["Displacement","Force",'Time'],skiprows=270, del
 
 #New addition to auto-generate the legend
 file_list=[file.replace("PLA uncut data\\", '') for file in file_list]
-
 
 file_list=[file.replace('.dat','') for file in file_list]
 file_list=[file.replace('s','#') for file in file_list]
@@ -229,7 +187,7 @@ for dataframe in data:
 
 stiles=["Displacement","Force",'Time']
 
-#Fucked up files need to be converted to numeric
+#Messed up files need to be converted to numeric
 for dataframe in data:
     for name in stiles:
         dataframe[name] = pd.to_numeric(dataframe[name],errors='coerce')
@@ -254,23 +212,12 @@ E=[]#list with all moduli
 for index, dataframe in enumerate(data):
     fig = plt.figure(figsize=(5,5),dpi=300)
     plt.plot(data[index]['Strain'],data[index]['Stress'],label=file_list[index],linestyle='-', marker=markers[index], mfc='w', markersize=6)
-  
-    # Axes limits. Adjust accordingly.
-    # plt.xlim([0.0012,0.009])
-    # plt.ylim([-2,15])
 
     # Ticks
     plt.minorticks_on() #uses minor ticks
-    # plt.tick_params(direction='in',right=False, top=False) #Obsolete with following lines. Places MAJOR TICKS only on sides & handles direction. Avoid using it.
-    # plt.tick_params(labelsize=10) #Changes values' font, which we locked earlier.
-    # plt.tick_params(labelbottom=True, labeltop=False, labelright=False, labelleft=True) #Turns on/off axis values on all 4 sides
-    # xticks = np.arange(1, 100,10) #Numpy to place ticks in certain positions X axis
-    # yticks = np.arange(0,160.1,4) #Numpy to place ticks in certain positions Y axis
-    
+
     plt.tick_params(direction='in',which='minor', length=2, bottom=True, top=False, left=True, right=False) #Properly handles minor ticks
     plt.tick_params(direction='in',which='major', length=4, bottom=True, top=False, left=True, right=False)# Bot left by default True if memory serves me. All change with =False though.
-    #plt.xticks(xticks) #Plots ticks
-    #plt.yticks(yticks) #Plots ticks
     
     #Plots grid in the background
     plt.grid(True,linestyle='dashed', linewidth='0.3', color='grey',alpha=0.8)
@@ -282,12 +229,10 @@ for index, dataframe in enumerate(data):
     # Legend
     plt.legend(frameon=False)  # Adds the legend. loc='best',bbox_to_anchor=(1,0.85)
    
-    #My linear regression that works. Drawn in the same graph with the linear region of the curve.
-    x_data=data[index]['Strain'].to_numpy()
-    y_data=data[index]['Stress'].to_numpy()
-    
    
     #Numpy least squares.
+    x_data=data[index]['Strain'].to_numpy()
+    y_data=data[index]['Stress'].to_numpy()
     A = np.vstack([x_data, np.ones(len(x_data))]).T
     m,c=np.linalg.lstsq(A,y_data,rcond=None)[0]
     slope=round(m/1000,3)
@@ -308,13 +253,10 @@ import matplotlib as mpl
 import seaborn as sns
 import glob
 import os
-
 from scipy.optimize import curve_fit
 from sklearn.linear_model import LinearRegression
-
 from io import StringIO
 from natsort import natsorted
-
 from statistics import mean
 import statistics
 from statistics import stdev
